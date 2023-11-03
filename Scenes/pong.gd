3da389e9fe2da5d813fd7549945e536885a5a4a9
Zfuhrman93 <leon378900@gmail.com>
2023-11-03 08:25:41 -0400

    Added reset fuction that when R is pressed will reset the ball and paddles to their default positions and reset the scores to 0.

---
+++ Scenes/pong.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var player = $"Paddle Player"
+@onready var AI = $"Paddle AI"
+@onready var ball = $Ball
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("Reset"):
+		_reset()
+
+func _reset():
+	Scores.player_score = 0
+	Scores.AI_score = 0
+	player.global_position.y = Intial.position_y
+	AI.global_position.y = Intial.position_y
+	ball.global_position = Intial.ball_position
---
+++ Scripts/intial.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var position_y = 200
+var ball_position = Vector2(300, 200)
